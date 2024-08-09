caf15790adbfa20a72a6ff2858bd211ae89efc1c
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-08-09 13:17:48 -0700

    npm

---
+++ 23. Node/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log("My name is " + sillyName);
+
+import generateSuperHero from "superheros";
+var superHero = generateSuperHero();
+
+console.log("My superhero name is " + superHero);
