6de61dbe8dd2d55df8faa2fe1a0035c70723c519
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-08-15 13:48:35 -0700

    act

--- 24. Express with Node/3.2 HTTP Requests/index.js
+++ 24. Express with Node/3.2 HTTP Requests/index.js
@@ -2,18 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello!</h1>");
-})
+app.get("/", (req, res) => { //Requests the home page
+    res.send("Hi, world!");
+});
 
 app.get("/contact", (req, res) => {
-    res.send("This is my contact page!")
+    res.send("My number is 6674511820");
 });
 
 app.get("/about", (req, res) => {
-    res.send("This is my page!")
+    res.send("My name is Claudia");
 });
 
 app.listen(port, () => {
-    console.log("Server running on port 3000.")
-})
+    console.log("server working!");
+});
