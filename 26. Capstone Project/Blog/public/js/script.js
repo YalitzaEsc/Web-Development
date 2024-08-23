d69aadd3988fc4997c8492ee9b9f8636ee845d8d
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-08-22 20:03:58 -0700

    avance project

--- 26. Capstone Project/Blog/public/js/script.js
+++ 26. Capstone Project/Blog/public/js/script.js
@@ -1,7 +1,24 @@
-function makeEditable(index){
-    var paragraph = document.getElementById("paragraph" + index);
-    var title = document.getElementById("title" + index);
-    paragraph.contentEditable = true;
-    title.contentEditable = true;
-    paragraph.focus();
-}
+$(document).on("click", ".edit", function() {
+
+    var id = $(this).attr("id");
+
+    var editableTitle = document.getElementById("title-" + id);
+    editableTitle.setAttribute("contenteditable", "true");
+
+    var editableParagraph = document.getElementById("paragraph-" + id);
+    editableParagraph.setAttribute("contenteditable", "true");
+    editableParagraph.focus();
+    
+    });
+    
+
+$(document).on("click", ".delete", function() {
+    var id = $(this).attr("id");
+
+    document.getElementById("title-" + id).remove();
+    document.getElementById("paragraph-" + id).remove();
+    document.getElementById("edit-" + id).remove();
+    document.getElementById(id).remove();
+    
+    
+})
