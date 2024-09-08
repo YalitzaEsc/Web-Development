893d67ae1055a4508ab9a8eaa5cf7174bba9dde2
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-09-07 20:56:44 -0700

    up

--- 33. PostgreSQL/8.3 Travel Tracker/index.js
+++ 33. PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -7,9 +8,36 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "98490133",
+  port: 5432,
 });
+
+db.connect((err) =>{
+  if(err){
+    console.log(err.stack)
+  } else {
+    console.log("Connected.")
+  }
+});
+
+
+app.get("/", async (req, res) => {
+
+  let consult = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+
+  consult.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+
+  res.render("index.ejs", {countries: countries, total: countries.length})
+  db.end();
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
