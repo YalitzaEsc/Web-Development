b10ee2bed406b4e2d32f01e8725680319655839f
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-09-11 16:51:11 -0700

    add

--- 34. Authentication and Security/9.3+Sessions+and+Cookies/index.js
+++ 34. Authentication and Security/9.3+Sessions+and+Cookies/index.js
@@ -10,15 +10,20 @@ const app = express();
 const port = 3000;
 const saltRounds = 10;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 // Always inicialize session before passport
 app.use(session({
   secret: "TOPSECRET",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // A day before the cookie expires
+  }
 })
 );
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,10 +42,18 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/login", passport.authenticate("local", {
-  successRedirect: "/secrets",
-  failureRedirect: "/login"
-}));
+app.get("/login", (req, res) => {
+  res.render("login.ejs")
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
@@ -72,12 +85,15 @@ app.post("/register", async (req, res) => {
         if (err) {
           console.error("Error hashing password:", err);
         } else {
-          console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("secrets.ejs");
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/secrets");
+          });
         }
       });
     }
@@ -86,47 +102,49 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secrets",
+  failureRedirect: "/login"
 
-});
-
-
-passport.use(new Strategy(async function verify(username, password, cb) {
+  }));
 
 
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      username,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedHashedPassword = user.password;
-      bcrypt.compare(password, storedHashedPassword, (err, result) => {
-        if (err) {
-          return cb(err)
-        } else {
-          if (result) {
-            return cb(null, user)
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            //Error with password check
+            console.error("Error comparing passwords:", err);
+            return cb(err);
           } else {
-            return cb(err)
+            if (valid) {
+              //Passed password check
+              return cb(null, user);
+            } else {
+              //Did not pass password check
+              return cb(null, false);
+            }
           }
-        }
-      });
-    } else {
-      return cb("User not found.")
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    return cb(err);
-  }
-
-}));
+  })
+);
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
 });
-
 passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
